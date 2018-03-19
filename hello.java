b46163f7f09820c92f86f60ae392960f9bc872d2
Sblancha <Sblancha76@Yahoo.com>
2018-03-19 09:56:27 -0400

    Changed from lastyear
    Asks user for name and outputs name in a string.

--- hello.java
+++ hello.java
@@ -5,13 +5,11 @@ import java.util.Scanner;
 public class hello {
 
     public static void main(String [] args) {
-	
-		Scanner input = new Scanner(System.in);
-	
-		String name;
-        System.out.println("Enter your name");
-		name = input.next();
-        System.out.println("Hello " + name + ", my name is Scott");
 
+	Scanner scnr = new Scanner(System.in);		
+	String name = "";
+	System.out.println("Enter your name.");
+	name = scnr.nextLine();	
+	System.out.println("Hello " + name + ", my name is Scott");
     }
 }
